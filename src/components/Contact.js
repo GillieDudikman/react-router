64e55a380fe42ea7ddb67d630c9293e1ee1aefc7
gillie dudikman <gilliedudik@gmail.com>
2023-09-03 16:33:10 +0300

    fix form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,18 +8,19 @@ const Contact = () => {
     return (
         <div className="contact">
             <h1>Contact Us</h1>
-            <Form method="post" action="/react-router/help/contact">
+            <Form method="post" replace>
                 <label>
                     <span>Your Email:</span>
                     <input type="email" name="email"
                            required placeholder="name@examle.com"/>
+                    {data && data.email !== "" ? (<span className="flex mt-3 flex-col text-red-600 font-extrabold">{data.email}</span>) : ''}
                 </label>
                 <label>
                     <span>Your message:</span>
                     <textarea name="message" required placeholder="Write your message here..."></textarea>
+                    {data && data.message !== "" ? (<span className="flex mt-3 flex-col text-red-600 font-extrabold ">{data.message}</span>) : ''}
                 </label>
                 <button type="submit">Submit</button>
-                {data && data.error && (<p>{data.error}</p>)}
             </Form>
         </div>
     )
@@ -27,8 +28,8 @@ const Contact = () => {
 
 export default Contact;
 
-export const contactAction = async ({ req }) => {
-    const data = await req.formData();
+export const contactAction = async ({ request }) => {
+    const data = await request.formData();
     const reg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
     const submission = {
@@ -46,6 +47,6 @@ export const contactAction = async ({ req }) => {
     if(Object.keys(submission.errors).length)
         return submission.errors;
 
-    return redirect("/react-router")
+    return redirect("/react-router/help")
 
 }
